2efbb2b34f8df43ab4c888b6c4b0a7f8e8ff3a0e
João Victor <joao.lenny100@gmail.com>
2022-10-13 11:11:49 -0300

    Requisito 13
    - Configurando a page "Profile".

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Loading from '../components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isLoading: true,
+      userInfos: {},
+    };
+
+    this.loadUserInfos = this.loadUserInfos.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadUserInfos();
+  }
+
+  async loadUserInfos() {
+    const userInfos = await getUser();
+    this.setState({ isLoading: false, userInfos });
+  }
+
   render() {
+    const { isLoading, userInfos } = this.state;
+
     return (
-      <div data-testid="page-profile"><Header /></div>
+      <div data-testid="page-profile">
+        <Header />
+
+        {
+          isLoading ? <Loading /> : (
+            <div>
+              <img
+                alt={ `Foto de ${userInfos.name}` }
+                data-testid="profile-image"
+                src={ userInfos.image }
+              />
+
+              <Link className="album" to="/profile/edit">
+                Editar perfil
+              </Link>
+
+              <h2>Nome:</h2>
+              <span>{ userInfos.name }</span>
+
+              <h2>E-Mail:</h2>
+              <span>{ userInfos.email }</span>
+
+              <h2>Descrição:</h2>
+              <p>{ userInfos.description }</p>
+            </div>
+          )
+        }
+      </div>
     );
   }
 }
